9387f815df967c82616fd3b9937f9817d093bcb8
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-15 21:16:59 +0200

    Fixed Bug

--- users.php
+++ users.php
@@ -34,6 +34,9 @@
       /* LIST USER BY PAGE 1 */
       }else{
         $res = $_user->listUsers($postBody);
+        print_r("CUANTO VALE EN EL RES");
+        print_r($res);
+
       }
       http_response_code(200);
       echo json_encode($res);
