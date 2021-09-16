92f6e972aa406ec223851850e3be1613139a2678
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-16 12:52:54 +0200

    Fix Cors

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
   require_once "class/connection/connection.php";
+  header('Access-Control-Allow-Origin: *');
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
   $connection = new connection;
 
