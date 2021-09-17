1850a5ecb01df60679f8fb7eb8fbcf1f9ed3f584
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-17 09:46:35 +0200

    fix cors

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require_once "class/connection/connection.php";
   header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Token");
   header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
   $connection = new connection;
--- tasks.php
+++ tasks.php
@@ -11,7 +11,7 @@
 
   header('content-Type: application/json');
   header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Token");
   header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
   switch ($resquest) {
