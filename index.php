12ce89767274d44cbbbbf92945370da3d829b25d
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-18 17:04:57 +0200

    Fix API

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Token");
   header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
-  $_response = new response;
-  $connection = new connection;
 
-  header('content-Type: application/json');
-  $data = $_response->error_200();
-  echo json_encode($data, JSON_UNESCAPED_UNICODE);
+  $connection = new connection;
 
 ?>
